43168a2bd491736f96b67eb59e2f19c2064bdc36
AlexanderSolman <alexander.solman.andersson@gmail.com>
2024-08-06 16:03:11 +0200

    Added functionality

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func addition(num1, num2 int) int {
+    return num1 + num2
+}
+
+func subtraction(num1, num2 int) int {
+    return num1 - num2
+}
+
+func multiplication(num1, num2 float64) float64 {
+    return num1 * num2
+}
+
+func dividen(num1, num2 float64) float64 {
+    return num1 / num2
+}
+
+func assertType(i interface{}) error {
+    if _, ok := i.(int); !ok { return fmt.Errorf("wrong type") }
+    return nil
+}
+
+func main() {
+    num1 := 10
+    num2 := 10
+
+    for {
+        if err := assertType(num1); err != nil { panic(err) }
+        if err := assertType(num2); err != nil { panic(err) }
+
+        fmt.Printf("%v + %v = %v\n", num1, num2, addition(num1, num2))
+        fmt.Printf("%v - %v = %v\n", num1, num2, subtraction(num1, num2))
+        fmt.Printf("%v * %v = %f\n", num1, num2, multiplication(float64(num1), float64(num2)))
+        fmt.Printf("%v / %v = %f\n", num1, num2, dividen(float64(num1), float64(num2)))
+        time.Sleep(5 * time.Second)
+    }
+}
